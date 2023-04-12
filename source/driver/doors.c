07ac492b0547413fe8a28e62c2a29571588bd1cc
krymskjm <125660124+krymskjm@users.noreply.github.com>
2023-04-12 23:05:27 +0200

    Add files via upload
    When floor_hit: elevator doors doesnt close when obstruction lamp is high

--- source/driver/doors.c
+++ source/driver/doors.c
@@ -5,7 +5,7 @@ void open_doors(ElevatorState * e) {
         time_t curr_ti;
         start_timer(&curr_ti);
         elevio_doorOpenLamp(1);       // turn on doorLamp
-        while (!timer_3_sec(curr_ti)) {
+        while (!timer_3_sec(curr_ti) || elevio_obstruction()) {     // turn doorlamp off when timer and no obstruction
             button_poller(e);       // handle buttons while doors are open
         }
         elevio_doorOpenLamp(0);
